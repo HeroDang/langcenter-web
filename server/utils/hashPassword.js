f72c957ed2b929ac6cbf22d1646198b6199b3be9
DangHung <danghungabt.bte@gmail.com>
2023-04-21 14:31:04 +0700

    fix bug yarn start

--- server/utils/hashPassword.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const hash = text => {
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(text, salt);
-  return hash;
-};
-
-module.exports = hash;
